708a950b061d37d19e4357b2a9bcffc79c0fb4dd
TylerNichols <tnichols@udel.edu>
2015-10-03 15:07:25 -0400

    Added non RE parsing
    Parsing professors from Course Search page is now done through CSS path
    using jquery instead of Regular Expression parsing raw text (less error
    prone).

--- myscript.js
+++ myscript.js
@@ -1,3 +1,5 @@
+
+
 //Content script Professor->ProfessorLink
 //Will parse professors last names, not including the first name
 //initially -- this is because first names can differ
@@ -10,6 +12,7 @@
 var urlFirstHalf = "http://www.ratemyprofessors.com/search.jsp?queryBy=teacherName&schoolName=university+of+delaware&queryoption=HEADER&query="
 var urlSecondHalf = "&facetSearch=true"
 
+
 //<span class="main">Lynch, T.G.</span> RMP page title
 
 //Returns the written TEXT on the page with no formatting.
@@ -21,10 +24,11 @@ function getHTML(){
 	return document.body.outerHTML
 }
 
+
 var pageText = getText();
-console.log(pageText);
+//console.log(pageText);
 var pageHTML = getHTML();
-console.log(pageHTML);
+//console.log(pageHTML);
 
 //Regular expresion to match Sometext,Someletter -- format for professor names
 var myRe = /[a-zA-Z]+,[^ ]/g;
@@ -34,6 +38,30 @@ var array_matches = pageText.match(myRe);
 
 console.log(array_matches.length);
 console.log(array_matches);
+
+//Non Regular Expression method
+//Initializing Matching array, Class row index, and Proff. string
+var Matches = [];
+var trIndex = 1;
+var professorString = ""
+
+while(professorString!=null){
+	professorString = $("#container > table > tbody > tr:nth-child(" + trIndex + ") > td:nth-child(8)").text();
+	//when there are no more valid professor rows, professorString = ""
+	if(professorString == ""){
+		professorString = null
+	}
+	else{
+		//rid of whitespace and add to Matches array
+		professorString = professorString.replace(/(\r\n|\n|\r)/gm, '');
+		Matches[trIndex] = professorString
+	}
+	trIndex++;
+}
+
+console.log(Matches);
+
+
 
 
 //Regular expression includes firstname(we want this),LastInitial(dont want this) right now
